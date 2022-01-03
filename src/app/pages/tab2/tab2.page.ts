6ca8f2876b3ea8cd36b6bab845e5e9543bdc5e9e
eideard-hm <ehhernandez81@misena.edu.co>
2022-01-03 11:04:03 -0500

    Se agrego el tab de categorias

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
 
+  public categories: string[] = ['business','entertainment','general','health','science','sports','technology'];
+  public selectedCategory = this.categories[0];
+
   constructor() {}
 
+  segmentChanged(category: any) {
+    console.log(category);
+  }
 }
